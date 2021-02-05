a0960099e9d119240f9585c344587e6a486c438a
Tenz331 <61831296+Tenz331@users.noreply.github.com>
2021-02-05 17:59:13 +0100

    Add files via upload

---
+++ src/main/java/entity/Animal.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author melo-
+ */
+@Entity
+public class Animal implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String type;
+
+    public Animal() {
+    }
+
+    public Animal(String type, String sound) {
+        this.type = type;
+        this.sound = sound;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getSound() {
+        return sound;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setSound(String sound) {
+        this.sound = sound;
+    }
+    private String sound;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    
+    
+}
